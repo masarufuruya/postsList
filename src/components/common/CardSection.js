d1d340f298c08ce03bf4e653cfd11e2caac08b4f
古家大 <yannwennri2@gmail.com>
2017-08-31 21:38:10 +0900

    ListViewを実装中

---
+++ src/components/common/CardSection.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  //row:横向き
+  //justifyContent: flex-start 先頭並び
+  containerStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative'
+  }
+};
+
+// export default CardSection;
+export { CardSection }
